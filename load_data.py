24a00161ccb20e2be31d80f8d7d73f70140f2669
LeBronLiHD <lebronlihd@outlook.com>
2021-11-21 20:36:42 +0800

    load data done

--- load_data.py
+++ load_data.py
@@ -4,15 +4,31 @@
 load data for training model
 """
 
-import numpy
 import pandas
 import math
 import os
 import parameters
+from pandas import read_csv
+from matplotlib import pyplot
+import numpy as np
 
 
 def load_train_data(file_path):
     print("path ->", file_path)
+    train_data = []
+    number_of_train = len(parameters.G_DataTrainFile)
+    test_data = []
+    number_of_test = len(parameters.G_DataTestFile)
+    for i in range(number_of_train):
+        train_data.append(os.path.join(file_path, parameters.G_DataTrainFile[i]))
+    for i in range(number_of_test):
+        test_data.append(os.path.join(file_path, parameters.G_DataTestFile[i]))
+    x_train, y_train = np.loadtxt(train_data[0], dtype=np.float32), \
+                       np.loadtxt(train_data[1], dtype=np.float32)
+    x_test = np.loadtxt(test_data[0], dtype=np.float32)
+    print("x_train.shape ->", x_train.shape)
+    print("y_train.shape ->", y_train.shape)
+    print("x_test.shape  ->", x_test.shape)
 
 
 if __name__ == '__main__':
--- parameters.py
+++ parameters.py
@@ -7,8 +7,11 @@ global parameters of the project, lkjnb!!!!
 G_ModelSave = "./model"
 G_DataPath = "./data"
 
-G_DataFile = [
-    "Btest_input.txt",
+G_DataTestFile = [
+    "Btest_input.txt"
+]
+
+G_DataTrainFile = [
     "Btrain_input.txt",
     "Btrain_output.txt"
 ]
